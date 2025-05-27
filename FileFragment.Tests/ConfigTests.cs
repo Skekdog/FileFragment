72cb361de8defaf70b69d6da225b8dea07ed9ca9
Skekdog <47750815+Skekdog@users.noreply.github.com>
2025-05-27 17:30:35 +0100

    Added tests to Config

---
+++ FileFragment.Tests/ConfigTests.cs
@@ -0,0 +1,34 @@
+﻿using FileFragment.Core;
+
+namespace FileFragment.Tests;
+
+public sealed class ConfigTests : IDisposable
+{
+    private readonly uint _originalPacketSize = Config.PacketSize;
+
+    public ConfigTests()
+    {
+        _originalPacketSize = Config.PacketSize;
+    }
+
+    public void Dispose()
+    {
+        Config.PacketSize = _originalPacketSize;
+    }
+
+    [Fact]
+    public void SetPacketSize_CannotBeLessThanOrEqualToOverhead()
+    {
+        Assert.Throws<Exception>(() =>
+        {
+            Config.PacketSize = (uint)FirstPacket.PacketOverhead;
+        });
+    }
+
+    [Fact]
+    public void SetPacketSize_CanBeGreaterThanOverhead()
+    {
+        Config.PacketSize = (uint)FirstPacket.PacketOverhead + 1;
+        Assert.Equal((uint)FirstPacket.PacketOverhead + 1, Config.PacketSize);
+    }
+}
