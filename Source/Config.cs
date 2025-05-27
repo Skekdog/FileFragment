8ae8561d3dcebc8beaf8799f7777572be1b1535e
Skekdog <47750815+Skekdog@users.noreply.github.com>
2025-05-27 13:09:23 +0100

    initial commit 3

--- Source/Config.cs
+++
@@ -1,6 +0,0 @@
-namespace FileFragmentV2.Source;
-
-public static class Config
-{
-	public static uint PacketSize { get; set; } = 50;
-}
--- Source/HexString.cs
+++
@@ -1,9 +0,0 @@
-namespace FileFragmentV2.Source;
-
-public static class HexString
-{
-	public static string ByteArrayToHexString(byte[] bytes)
-	{
-		return BitConverter.ToString(bytes).Replace("-", " ");
-	}
-}
