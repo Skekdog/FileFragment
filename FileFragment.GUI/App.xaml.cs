ac6220e5932ed9c703c6e729dc0fe71007e13e08
Skekdog <47750815+Skekdog@users.noreply.github.com>
2025-05-28 22:48:43 +0100

    Implemented the fragment page

--- FileFragment.GUI/App.xaml.cs
+++ FileFragment.GUI/App.xaml.cs
@@ -25,10 +25,10 @@ namespace FileFragment.GUI
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs args)
         {
-            m_window = new MainWindow();
-            m_window.Activate();
+            Window = new MainWindow();
+            Window.Activate();
         }
 
-        private Window? m_window;
+        public static Window? Window;
     }
 }
--- FileFragment.GUI/Pages/FragmentPage.xaml.cs
+++ FileFragment.GUI/Pages/FragmentPage.xaml.cs
@@ -1,4 +1,10 @@
+using System;
+using FileFragment.Core;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -10,9 +16,74 @@ namespace FileFragment.GUI.Pages
     /// </summary>
     public sealed partial class FragmentPage : Page
     {
+        private string selectedInputFile = "";
+        private string selectedOutputDirectory = "";
+
+        private nint? hwnd;
+
         public FragmentPage()
         {
             InitializeComponent();
+        }
+
+        private void SetStatusText(string text) => StatusTextBlock.Text = text;
+
+        private void OnFragmentClicked(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(selectedInputFile)) { SetStatusText("Error: No input file selected"); return; }
+            if (string.IsNullOrEmpty(selectedOutputDirectory)) { SetStatusText("Error: No output directory selected"); return; }
+
+            try
+            {
+                _ = uint.TryParse(PacketSizeTextBox.Text, out uint packetSize);
+                if (packetSize == 0) throw new Exception("Invalid packet size");
+
+                DiskOp.Fragment(packetSize, selectedInputFile, selectedOutputDirectory);
+
+                StatusTextBlock.Text = "Fragmentation complete!";
+            }
+            catch (Exception ex)
+            {
+                SetStatusText($"Error: {ex.Message}");
+            }
+        }
+
+        private async void OnSelectInputFileClicked(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker picker = new()
+            {
+                SuggestedStartLocation = PickerLocationId.DocumentsLibrary,
+            };
+
+            picker.FileTypeFilter.Add("*");
+
+            hwnd ??= WindowNative.GetWindowHandle(App.Window);
+            InitializeWithWindow.Initialize(picker, hwnd!.Value);
+
+            StorageFile file = await picker.PickSingleFileAsync();
+
+            if (file is null) return;
+
+            selectedInputFile = file.Path;
+            SelectFileTextBlock.Text = $"Selected: {file.Path}";
+        }
+
+        private async void OnSelectOutputDirectoryClicked(object sender, RoutedEventArgs e)
+        {
+            FolderPicker picker = new()
+            {
+                SuggestedStartLocation = PickerLocationId.DocumentsLibrary,
+            };
+
+            hwnd ??= WindowNative.GetWindowHandle(App.Window);
+            InitializeWithWindow.Initialize(picker, hwnd!.Value);
+
+            StorageFolder folder = await picker.PickSingleFolderAsync();
+
+            if (folder is null) return;
+
+            selectedOutputDirectory = folder.Path;
+            SelectOutputDirectoryTextBlock.Text = $"Selected: {folder.Path}";
         }
     }
 }
